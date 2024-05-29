9603ec7b4c08787ac50c1bdacc01baabffd60fdc
Gustavo <gustavo.reis98@gmail.com>
2024-05-29 19:28:48 -0300

    alteração de diretoriol

--- PHP-CONCEITOS/exercicio2.php
+++ PHP-CONCEITOS/exercicio2.php
@@ -1,22 +1,22 @@
 <?php
-// Solicitando ao usuário que digite dois números
+// Solicitar ao usuário que digite dois números
 echo "Digite o primeiro número (a): ";
-$a = trim(fgets(STDIN));  // Lendo entrada do usuário via linha de comando
+$userA = trim(fgets(STDIN));  // Lendo entrada do usuário via linha de comando
 echo "Digite o segundo número (b): ";
-$b = trim(fgets(STDIN));
+$userB = trim(fgets(STDIN));
 
 // Garantindo que 'a' seja menor ou igual a 'b'
-while ($a > $b) {
+while ($userA > $userB) {
     echo "O primeiro número deve ser menor ou igual ao segundo número. Por favor, digite novamente.\n";
     echo "Digite o primeiro número (a): ";
-    $a = trim(fgets(STDIN));
+    $userA = trim(fgets(STDIN));
     echo "Digite o segundo número (b): ";
-    $b = trim(fgets(STDIN));
+    $userB = trim(fgets(STDIN));
 }
 
 // Calculando a soma dos números ímpares no intervalo [a, b]
 $soma = 0;
-for ($i = $a; $i <= $b; $i++) {
+for ($i = $userA; $i <= $userB; $i++) {
     if ($i % 2 !== 0) {  // Verificando se o número é ímpar
         $soma += $i;
     }
