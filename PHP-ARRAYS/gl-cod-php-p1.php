34bdabce22d04bedb9cc2fb5af93a19f385c6675
Gustavo <gustavo.reis98@gmail.com>
2024-06-04 23:13:23 -0300

    Feito exercio 8 do php-p1

--- PHP-ARRAYS/gl-cod-php-p1.php
+++ PHP-ARRAYS/gl-cod-php-p1.php
@@ -23,3 +23,23 @@ for ($i = 1; $i <= 10; $i++) {
     echo $numero . " x " . $i . " = " . ($numero * $i) . "\n";
 }
 ?>
+
+<?php
+$numeros = array();
+
+$soma = 0;
+
+for ($i = 0; $i < 5; $i++) {
+    echo "Digite o " . ($i + 1) . "º número: ";
+    $numeros[$i] = intval(fgets(STDIN));
+}
+
+// Calcula a soma dos elementos que são pares
+for ($i = 0; $i < 5; $i++) {
+    if ($numeros[$i] % 2 == 0) {
+        $soma += $numeros[$i];
+    }
+}
+
+echo "A soma dos elementos pares é: " . $soma . "\n";
+?>
